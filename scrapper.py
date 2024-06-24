950b95f377e816fbcdaa30ce6e35e8f10e0e4b8f
kaymeric <aymeric.kreuzer@ynov.com>
2024-06-24 16:01:59 +0200

    hotfix

--- scrapper.py
+++ scrapper.py
@@ -8,9 +8,11 @@ def scrap(url : str) -> bs:
     if response.status_code != 200:
         raise Exception(f"Error {response.status_code} while trying to fetch data : {response.text}")
 
-    soup_data = text_to_soup(response.text)
+    # soup_data = text_to_soup(response.text)
 
-    return soup_data
+    # return soup_data
+    
+    return response.text
 
 def text_to_soup(text : str) -> bs:
     return bs(text, 'html.parser')
