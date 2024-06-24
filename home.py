dcc357676b79bb179a61ae8ef403b2893f721aa6
kaymeric <aymeric.kreuzer@ynov.com>
2024-06-24 16:33:58 +0200

    hotfix

--- home.py
+++ home.py
@@ -1,8 +1,11 @@
 def index(req):
     data = ""
     
-    with open("data.txt", "r") as fichier:
+    with open("data.txt", "r+", encoding="utf-8") as fichier:
         # Lire le contenu du fichier
         data = fichier.read()
     
     return data
+
+if __name__ == "__main__":
+    print(index(None))
